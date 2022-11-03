796c4e01224247cc77224e92a9ec41739694d720
jonyyy1 <jonathan.quilca@utec.edu.pe>
2022-11-03 17:40:34 -0500

    actualizando la estructura v2

--- clases/Pizarra.h
+++ clases/Pizarra.h
@@ -6,19 +6,39 @@
 #define PROYECTOGIMP_PIZARRA_H
 #include <vector>
 #include "CFiguraGeometrica.h"
+#include "../constantes/Matriz.h"
+
 
 class Pizarra {
     vector<CFiguraGeometrica*> figuras;
 public:
     Pizarra(){}
-    void agregar_figura(CFiguraGeometrica* f){
+    void agregar(CFiguraGeometrica* f){
         figuras.push_back(f);
     }
-    void eliminar(int numero){
+    void eliminar(int id){
         for(auto i: figuras){
-
+            if(i->get_id()==id){
+                figuras.erase(figuras.begin() + id);
+                cout <<"se elimino el id : "<< id<<endl;
+            }
         }
     }
+    void girar(){
+
+    }
+    void redimensionar(){
+
+    }
+    void mostrar(){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                cout << matriz[i][j]<<" ";
+            }
+            cout << endl;
+        }
+    }
+/**/
 };
 
 
--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 #include "clases/CLinea.h"
 #include "clases/Coordenada.h"
 #include <vector>
-
+#include "clases/Pizarra.h"
 using namespace std;
 int id = 0;
+vector<CFiguraGeometrica*> figuras;
 template<typename T>
 void imprimir(T d){
     cout << "id : "<<d->get_id()<<endl;
     cout <<"Figura : "<<d->get_nombre_figura()<<endl;
     for(auto i : d->get_coordenadas()){cout <<"("<<i->get_x()<<","<<i->get_y()<<")"<<endl;}
 }
+
+
 int autoincrement_id(){
     return id+=1;
 }
@@ -54,12 +57,41 @@ void simulation_GIMP(){
     }else{
         //PARA EL TRIANGULO
     }
+    //AGREGAR A LA PIZARRA
+    figuras.push_back(f);
     //REPORTE
     imprimir(f);
 
 }
 int main() {
-    simulation_GIMP();
-    simulation_GIMP();
+    int numero_a_elegir = 0;
+    int continuar = 0;
+    cout <<"BIENVENIDO A GIMP"<<endl;
+    cout <<"MENU"<< endl;
+    Pizarra* pizarra;
+    while(true) {
+        cout <<"1.-AGREGAR"<< endl;
+        cout <<"2.-ELIMINAR"<< endl;
+        cout <<"3.-GIRAR"<< endl;
+        cout <<"4.-REDIMENSIONAR"<<endl;
+        cout << "5.-MOSTRAR"<<endl;
+        cout <<"6.-SALIR"<<endl;
+        cin >> numero_a_elegir;
+        if (numero_a_elegir==1){
+            pizarra->mostrar();
+        }else if(numero_a_elegir==2){
+            //eliminar();
+        }else if(numero_a_elegir==3){
+            //girar();
+        }else if(numero_a_elegir==4){
+            //redimensionar();
+        }else if(numero_a_elegir==5){
+            //mostrar();
+        }else if(numero_a_elegir==6){
+            break;
+        }else{
+            cout <<"opcion incorrecta, vuelva a elegir otro numero"<< endl;
+        }
+    }
     return 0;
 }
